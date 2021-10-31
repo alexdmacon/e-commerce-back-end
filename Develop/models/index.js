001427ce45994cf572c3a92b69d7129f9b69fbf1
alexdmacon <alexdmacon@gmail.com>
2021-10-31 17:45:59 -0500

    wrote index.js for models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,35 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+}),
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongstoMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'product_tags',
+})
 
 // Tags belongToMany Products (through ProductTag)
+
+ProductTag.belongstoMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'product_tags',
+})
 
 module.exports = {
   Product,
