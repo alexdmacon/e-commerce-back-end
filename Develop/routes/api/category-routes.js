83448307ae6b6ec0508f89a373ec118f27374d36
alexdmacon <alexdmacon@gmail.com>
2021-11-02 20:25:43 -0500

    wrote category api route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,22 +3,56 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findall({
+      include: [{model: Product}]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+
+// NEED TO COME BACK TO THIS BECASUE I'M NOT REALLY SURE WHAT I'M DOING
+router.put('/:id', async (req, res) => {
+  const id = req.params.id;
+  const { userInput } = req.body;
+  try {
+  const categoryData = await Category.findOne({where: { id }})
+  categoryData.userInput = userInput;
+  await categoryData.save();
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // update a category by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
