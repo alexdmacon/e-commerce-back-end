d2439a2a02da6ada72266d6ea4ff85097b295309
alexdmacon <alexdmacon@gmail.com>
2021-11-04 19:55:51 -0500

    tag routes all work, collection created in insomnia

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,36 +1,41 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-  // find all tags
-  // be sure to include its associated Product data
-router.get('/', async (req, res) => {
+// find all tags
+// be sure to include its associated Product data
+router.get("/", async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-     include: [{ model: Product }]
+      include: [{ model: Product }],
     });
     res.status(200).json(tagData);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-router.get('/:id', async (req, res) => {
+// find a single tag by its `id`
+// be sure to include its associated Product data
+router.get("/:id", async (req, res) => {
   try {
-    const tagData = await Tag.findbyPk(req.params.id, {
-      include: [{ model: Product }]
+    const tagData = await Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: {
+        model: Product,
+      },
     });
     res.status(200).json(tagData);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
-  // create a new tag
-router.post('/', async (req, res) => {
+// create a new tag
+router.post("/", async (req, res) => {
   try {
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
@@ -39,30 +44,36 @@ router.post('/', async (req, res) => {
   }
 });
 
-// update by id—AGAIN NOT REALLY SURE WHAT I'M DOING HERE, WILL NEED TO COME BACK.
-router.put('/:id', async (req, res) => {
-  const id = req.params.id;
-  const { userInput } = req.body;
+// update by id
+router.put("/:id", async (req, res) => {
   try {
-  const tagData = await Tag.findOne({where: { id }})
-  tagData.userInput = userInput;
-  await tagData.save();
+    const tagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-  // delete on tag by its `id` value
-router.delete('/:id', async (req, res) => {
+// delete on tag by its `id` value
+router.delete("/:id", async (req, res) => {
   try {
     const tagData = await Tag.destroy({
       where: {
         id: req.params.id,
-      }
+      },
     });
     res.status(200).json(tagData);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
